491b48c0ef6a9503103dc7fa5834a4e6b74d11c7
pvaldivia <pvaldivia1@mail.csuchico.edu>
2018-01-23 14:50:51 -0800

    Added some documentation to data.java

--- app/src/main/java/edu/pedro/shoppinglist/Data.java
+++ app/src/main/java/edu/pedro/shoppinglist/Data.java
@@ -1,27 +1,33 @@
 package edu.pedro.shoppinglist;
+/* Might need to change "edu.pedro" to your own domain name*/
 import android.content.Context;
 import android.content.SharedPreferences;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-//1
+
 public class Data {
 
     private Context context;
+    /* Might need to change "edu.pedro." to what every you used as your domain*/
     private static final String StoredStringsKey = "edu.pedro.shoppinglist.storedstringskey";
     private static final String PreferencesLocation = "edu.pedro.shoppinglist";
 
     public Data(Context c){
         context = c;
     }
-    //2
+    /* getStoredString
+        This function simply returns a list of strings (Shopping items) in an array of string
+    */
     public ArrayList<String> getStoredStrings(){
         SharedPreferences preferences = context.getSharedPreferences(PreferencesLocation, Context.MODE_PRIVATE);
         Set<String> stringSet = preferences.getStringSet(StoredStringsKey, Collections.<String>emptySet());
         return new ArrayList<String>(stringSet);
     }
-    //3
+    /* setStoredStrings
+        This function takes in an array of strings as a parameter and stores them within the editor
+    */
     public void setStoredStrings(ArrayList<String> strings){
         SharedPreferences preferences = context.getSharedPreferences(PreferencesLocation, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
